cc78701edf1c35d10ac66f7e7fc1028752c7f97f
peter-rvb <peterbateson02@gmail.com>
2023-06-03 12:38:30 +0100

    Introducing Erlang

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express() 
+
 const posts = require('./posts')
+
+const erlang = require('erlang-c-js')
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs')
+app.use(logger)
+
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/posts', (req, res) => {
     res.json(posts)
@@ -10,5 +21,12 @@ app.get('/', (req, res) => {
     res.send('Api is runnning now.')
 })
 
+const erlangRouter = require('./routes/erlang')
+app.use('/erlang', erlangRouter)
 
-app.listen(process.env.PORT || 3000, () => console.log("Server is running"))
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
+app.listen(process.env.PORT || 3000, () => console.log("Relax, your server is running"))
